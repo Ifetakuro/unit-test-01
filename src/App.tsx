cdac7eb430d20c9c7b13f351aaf2db991c37abd2
oluwafisayo.takuro <oluwafisayo.takuro@interswitchng.com>
2023-11-30 12:37:52 +0100

    updated msw

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { AppProviders } from "./components/providers/app-providers";
 import { MuiMode } from "./components/mui/mui-mode";
+import { Users } from "./components/users/users";
 
 function App() {
   return (
     <AppProviders>
       <div className="App">
         <MuiMode />
+        <Users />
       </div>
     </AppProviders>
   );
